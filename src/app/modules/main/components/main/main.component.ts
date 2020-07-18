653694f0e5d386c092795c182c61fdd2ec74d7e8
Paweł Rabiczko <pawel.rabiczko@merixstudio.com>
2020-07-18 22:07:22 +0200

    cl erased

--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -11,8 +11,6 @@ export class MainComponent {
   constructor(private router: Router) { }
 
   isDashboard(): boolean {
-    console.log(this.router.url);
-
     return this.router.url === '/'
   }
 }
