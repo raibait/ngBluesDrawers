1bcbc15d78ca8c36df89325d82551585482dec46
Paweł Rabiczko <pawel.rabiczko@merixstudio.com>
2020-07-18 21:10:40 +0200

    hosting setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAK8uqTvoKLHIbeDZs1FOK-YrqNOtjwUoU",
+    authDomain: "bluesdrawers.firebaseapp.com",
+    databaseURL: "https://bluesdrawers.firebaseio.com",
+    projectId: "bluesdrawers",
+    storageBucket: "bluesdrawers.appspot.com",
+    messagingSenderId: "616873611676",
+    appId: "1:616873611676:web:13cfbcf26ce61bdfd0b859",
+    measurementId: "G-MC90JLV4RP"
+  }
 };
